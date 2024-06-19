fe31a04771e40d5d5e4d2abe373496d7a821a902
Muhammad Umair Mohsin <77578635+umairmohsin@users.noreply.github.com>
2024-06-20 01:38:23 +0300

    Add files via upload

---
+++ ingest.py
@@ -0,0 +1,26 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader, PDFMinerLoader
+from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain_community.vectorstores import Chroma
+from chromadb.config import Settings
+import os
+
+def main():
+    documents=[]
+    for root,dirs,files in os.walk("docs"):
+        for file in files:
+            if file.endswith(".pdf"):
+                print(file)
+                loader = PDFMinerLoader(os.path.join(root,file))
+    documents.extend(loader.load())
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size =500 , chunk_overlap =50)
+    texts = text_splitter.split_documents(documents)
+    #create embeddings
+    embeddings = SentenceTransformerEmbeddings(model_name ="all-MiniLM-L6-v2")
+    db = Chroma.from_documents(
+        texts,embeddings,persist_directory = "db"
+    )
+    
+if __name__ == "__main__":
+    main()    
+    
